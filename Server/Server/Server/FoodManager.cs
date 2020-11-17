09d2bd70e3dce1dfd29c87fe96b0b58811e59cd0
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-11-17 14:23:36 +0200

    refactor and adds logic to detect collisions

--- Server/Server/Server/FoodManager.cs
+++ Server/Server/Server/FoodManager.cs
@@ -7,16 +7,21 @@ using System.Threading;
 
 namespace Server
 {
-    public static class FoodCreator
+    public interface IFoodManager
     {
-        public static ConcurrentBag<Circle> spawnedFood = new ConcurrentBag<Circle>();
-        public static Vector2 CreateFood()
+        public List<Circle> SpawnedFood { get;}
+        public Vector2 CreateFood();
+    }
+    public class FoodManager : IFoodManager
+    {
+        public List<Circle> SpawnedFood => new List<Circle>();
+        public Vector2 CreateFood()
         {
             Random r = new Random();
             float x = r.Next(-50, 50);
             float y = r.Next(-50, 50);
             Vector2 foodPosition = new Vector2(x, y);
-            spawnedFood.Add(new Circle { position = foodPosition, radius = 0.2f });
+            SpawnedFood.Add(new Circle { position = foodPosition, radius = 50f });
             return foodPosition;
         }
     }
